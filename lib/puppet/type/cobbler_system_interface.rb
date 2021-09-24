6a267348e855b3155a45cbf65e18c5c0ae15e42d
Aran Cox <aran-cox@uiowa.edu>
2021-09-24 16:18:45 -0500

    add the rest of the parameters to cobbler_system_interface

--- lib/puppet/type/cobbler_system_interface.rb
+++ lib/puppet/type/cobbler_system_interface.rb
@@ -41,5 +41,141 @@ EOS
       desc:      'The name of the interface on the system.',
       behaviour: :namevar,
     },
+
+    #   "e": {
+    #   "bonding_opts": "",
+    #   "bridge_opts": "",
+    #   "cnames": [],
+    #   "connected_mode": false,
+    #   "dhcp_tag": "",
+    #   "dns_name": "",
+    #   "if_gateway": "",
+    #   "interface_master": "",
+    #   "interface_type": "",
+    #   "ip_address": "",
+    #   "ipv6_address": "",
+    #   "ipv6_default_gateway": "",
+    #   "ipv6_mtu": "",
+    #   "ipv6_prefix": "",
+    #   "ipv6_secondaries": [],
+    #   "ipv6_static_routes": [],
+    #   "mac_address": "48:03:73:34:13:99",
+    #   "management": false,
+    #   "mtu": "",
+    #   "netmask": "",
+    #   "static": true,
+    #   "static_routes": [],
+    #   "virt_bridge": "xenbr0"
+    # }
+    mac_address: {
+      type: 'Optional[String]',
+      desc: 'MAC Address of the interface.',
+      default: nil,
+    },
+    mtu: {
+      type: 'Optional[Integer]',
+      desc: 'MTU assigned to interface.',
+      default: nil,
+    },
+    ip_address: {
+      type: 'Optional[String]',
+      desc: 'IPV4 address assigned to interface.',
+      default: nil,
+    },
+    interface_type: {
+      type: 'Optional[String]',
+      desc: 'Interface type (na, bond, bridge, etc.)',
+      default: nil,
+    },
+    interface_master: {
+      type: 'Optional[String]',
+      desc: 'Interface master, presumably for bonded or bridged interfaces.',
+      default: nil,
+    },
+    bonding_opts: {
+      type: 'Optional[String]',
+      desc: 'Bond options for bonded interfaces.',
+      default: nil,
+    },
+    bridge_opts: {
+      type: 'Optional[String]',
+      desc: 'Bond options for bonded interfaces.',
+      default: nil,
+    },
+    management: {
+      type: 'Optional[Boolean]',
+      desc: 'Set as management interface.',
+      default: nil,
+    },
+    static: {
+      type: 'Optional[Boolean]',
+      desc: 'Staticly configure interface.',
+      default: nil,
+    },
+    netmask: {
+      type: 'Optional[String]',
+      desc: 'Subnet Mask.',
+      default: nil,
+    },
+    if_gateway: {
+      type: 'Optional[String]',
+      desc: 'Per-interface gateway.',
+      default: nil,
+    },
+    dhcp_tag: {
+      type: 'Optional[String]',
+      desc: 'DHCP tag.',
+      default: nil,
+    },
+    dns_name: {
+      type: 'Optional[String]',
+      desc: 'DNS Name.',
+      default: nil,
+    },
+    static_routes: {
+      type: 'Optional[Variant[Array[String],String]]',
+      desc: 'Static routes.',
+      default: nil,
+    },
+    virt_bridge: {
+      type: 'Optional[String]',
+      desc: 'Virt bridge.',
+      default: nil,
+    },
+    ipv6_address: {
+      type: 'Optional[String]',
+      desc: 'IPV6 Address.',
+      default: nil,
+    },
+    ipv6_prefix: {
+      type: 'Optional[String]',
+      desc: 'IPV6 Prefix.',
+      default: nil,
+    },
+    ipv6_secondaries: {
+      type: 'Optional[Array[String]]',
+      desc: 'IPV6 Secondaries.',
+      default: nil,
+    },
+    ipv6_mtu: {
+      type: 'Optional[Integer]',
+      desc: 'IPV6 MTU.',
+      default: nil,
+    },
+    ipv6_static_routes: {
+      type: 'Optional[Array[String]]',
+      desc: 'IPV6 Secondaries.',
+      default: nil,
+    },
+    ipv6_default_gateway: {
+      type: 'Optional[String]',
+      desc: 'IPV6 Default Gateway.',
+      default: nil,
+    },
+    cnames: {
+      type: 'Optional[Array[String]]',
+      desc: 'CNAMES.',
+      default: nil,
+    },
   },
 )
