1d07d109a7c5cb09d43d542567be6ceb130682fe
Aran Cox <aran-cox@uiowa.edu>
2021-10-28 15:07:49 -0500

    add implementation of profile

---
+++ lib/puppet/provider/cobbler_profile/cobbler_profile.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'puppet/resource_api/simple_provider'
+require 'xmlrpc/client'
+
+# Implementation for the cobbler_profile type using the Resource API.
+class Puppet::Provider::CobblerProfile::CobblerProfile < Puppet::ResourceApi::SimpleProvider
+  def initialize
+    @systems = []
+    unless @client
+      @client = XMLRPC::Client.new2(ENV['COBBLER_URI'])
+    end
+    unless @token and _version = @client.call('version',@token)
+      @token = @client.call('login', ENV['COBBLER_USERNAME'], ENV['COBBLER_PASSWORD'])
+    end
+    super()
+  end
+  def get(context)
+    context.debug('Returning system data')
+    unless @systems.length > 0 
+      gs = @client.call('get_systems')
+      context.debug("#{gs.inspect}")
+      simple_xlate = %w( name owners distro parent enable_menu autoinstall kernel_options kernel_options_post autoinstall_meta proxy repos comment enable_gpxe dhcp_tag server next_server filename name_servers name_servers_search )
+      
+      gs.each do |s|
+        st = { :ensure => 'present' }
+        simple_xlate.each do |x|
+          if s.has_key?(x)
+            st[x.to_sym] = s[x]
+          end
+        end
+        @systems << st
+      end
+      context.debug(@systems)
+    end
+    @systems
+  end
+  def create(context, name, should)
+    context.notice("Creating '#{name}' with #{should.inspect}")
+    values = {}
+    should.map do |k,v|
+      if v == '' or k == :ensure
+        next
+      end
+      values[k.to_s] = v
+    end
+    context.notice("#{values.inspect}")
+    @client.call('xapi_object_edit',
+                 'profile',
+                 name,
+                 'add',
+                 values,
+                 @token)
+  end
+  def update(context, name, should)
+    context.notice("Updating '#{name}' with #{should.inspect}")
+    values = {}
+    should.map do |k,v|
+      if k == :ensure or k == :name
+        next
+      end
+      values[k.to_s] = v
+    end
+    context.notice("#{values.inspect}")
+    @client.call('xapi_object_edit',
+                 'profile',
+                 name,
+                 'edit',
+                 values,
+                 @token)
+    context.debug("finished updating #{name}")
+  end
+
+  def delete(context, name)
+    context.notice("Deleting '#{name}'")
+    @client.call('xapi_object_edit',
+                 'profile',
+                 name,
+                 'remove',
+                 {},
+                 @token)
+  end
+end
---
+++ lib/puppet/type/cobbler_profile.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require 'puppet/resource_api'
+
+Puppet::ResourceApi.register_type(
+  name: 'cobbler_profile',
+  docs: <<-EOS,
+@summary a cobbler_profile type
+@example
+cobbler_profile { 'foo':
+  ensure => 'present',
+}
+
+This type provides Puppet with the capabilities to manage ...
+
+If your type uses autorequires, please document as shown below, else delete
+these lines.
+**Autorequires**:
+* `Package[foo]`
+EOS
+  features: [],
+  attributes: {
+    ensure: {
+      type: 'Enum[present, absent]',
+      desc: 'Whether this resource should be present or absent on the target system.',
+      default: 'present',
+    },
+    name: {
+      type: 'String',
+      desc: 'The name of the profile.',
+      behaviour: :namevar,
+    },
+    # General tab
+    owners: {
+      type: 'Optional[Variant[String,Array]]',
+      desc: 'Sets owners for the system.',
+      default: nil,
+    },
+    distro: {
+      type: 'Optional[String]',
+      desc: 'The distro associated with this profile.',
+      default: nil,
+    },
+    parent: {
+      type: 'Optional[String]',
+      desc: 'The parent profile associated with this profile.',
+      default: nil,
+    },
+    enable_menu: {
+      type: 'Optional[Boolean]',
+      desc: 'Enable PXE Menu?',
+      default: nil,
+    },
+    autoinstall: {
+      type: 'Optional[String]',
+      desc: 'Automatic installation template aka kickstart file.',
+      default: nil,
+    },
+    kernel_options: {
+      type: 'Optional[Hash]',
+      desc: 'Hash of kernel options to be passed during install.',
+      default: nil,
+    },
+    kernel_options_post: {
+      type: 'Optional[Hash]',
+      desc: 'Hash of kernel options to be used post install.',
+      default: nil,
+    },
+    autoinstall_meta: {
+      type: 'Optional[Hash]',
+      desc: 'Hash of autoinstall (kickstart) metadata.',
+      default: nil,
+    },
+    proxy: {
+      type: 'Optional[String]',
+      desc: 'Internal proxy to use.',
+      default: nil,
+    },
+    repos: {
+      type: 'Optional[Variant[Array[String],String]]',
+      desc: 'repos to use during install',
+      default: nil,
+    },
+    comment: {
+      type: 'Optional[String]',
+      desc: 'Cobbler system comment.',
+      default: nil,
+    },
+    # "Advanced Tab"
+    enable_gpxe: {
+      type: 'Optional[Variant[Boolean,String,Integer]]',
+      desc: 'enable gPXE support, can be inherited as well',
+      default: nil,
+    },
+    dhcp_tag: {
+      type: 'Optional[String]',
+      desc: 'dhcp tag',
+      default: nil,
+    },                        
+    server: {
+      type: 'Optional[String]',
+      desc: ' gPXE server',
+      default: nil,
+    },
+    next_server: {
+      type: 'Optional[String]',
+      desc: ' gPXE next server',
+      default: nil,
+    },
+    filename: {
+      type: 'Optional[String]',
+      desc: ' gPXE file name override',
+      default: nil,
+    },
+    # networking (global)
+    name_servers: {
+      type: 'Optional[Variant[Array[String],String]]',
+      desc: 'name server list',
+      default: nil,
+    },
+    name_servers_search: {
+      type: 'Optional[Variant[Array[String],String]]',
+      desc: 'name server domain search list',
+      default: nil,
+    },
+  },
+)
